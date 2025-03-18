2dddd2a570da4bf4d0c250aafdab0f51ceb402de
tediouscat <515889839@qq.com>
2025-03-18 23:30:01 +0800

    首页 登录页

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import Index from "@/frontend/index.vue";
+import Index from "@/pages/frontend/index.vue";
 import {createRouter,createWebHashHistory} from "vue-router";
+import Login from "@/pages/admin/login.vue";
 
 const routes = [
     {
@@ -8,7 +9,15 @@ const routes = [
         meta: {
             title: 'tediouscatblog首页'
         }
-    }
+    },
+    // 省略...
+    {
+        path: '/login', // 登录页
+        component: Login,
+        meta: {
+            title: 'TediouscatBlog 登录页'
+        }
+    },
 ]
 
 const routers =createRouter({
