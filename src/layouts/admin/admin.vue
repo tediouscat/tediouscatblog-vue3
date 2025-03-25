bd195d3edd114c900401313aac1fef2ecd7ee4e1
tediouscat <tediouscat@126.com>
2025-03-25 14:58:02 +0800

    使用 Transition 组件添加全局过渡动画

--- src/layouts/admin/admin.vue
+++ src/layouts/admin/admin.vue
@@ -21,9 +21,12 @@
         <!-- 主内容（根据路由动态展示不同页面） -->
         <router-view v-slot="{ Component }">
           <!-- max 指定最多缓存 10 个组件 -->
-          <KeepAlive :max="10">
-            <component :is="Component"></component>
-          </KeepAlive>
+          <Transition name="fade">
+            <!-- max 指定最多缓存 10 个组件 -->
+            <KeepAlive :max="10">
+              <component :is="Component"></component>
+            </KeepAlive>
+          </Transition>
         </router-view>
       </el-main>
 
@@ -57,5 +60,38 @@ const menuStore = useMenuStore()
 
 .el-footer {
   padding: 0!important;
+}
+
+/* 内容区域过渡动画：淡入淡出效果 */
+/* 刚开始进入时 */
+.fade-enter-from {
+  /* 透明度 */
+  opacity: 0;
+}
+
+/* 刚开始结束 */
+.fade-enter-to {
+  opacity: 1;
+}
+
+/* 刚开始离开 */
+.fade-leave-from {
+  opacity: 1;
+}
+
+/* 离开已结束 */
+.fade-leave-to {
+  opacity: 0;
+}
+
+/* 离开进行中 */
+.fade-leave-active {
+  transition: all 0.3s;
+}
+
+/* 进入进行中 */
+.fade-enter-active {
+  transition: all 0.3s;
+  transition-delay: 0.3s;
 }
 </style>
