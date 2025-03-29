4019e520c874f98abc91fe96da73f2cb8290dc05
tediouscat <515889839@qq.com>
2025-03-29 13:38:44 +0800

    文章分类：分页列表数据动态渲染

--- src/pages/admin/category-list.vue
+++ src/pages/admin/category-list.vue
@@ -10,12 +10,15 @@
         <el-text>创建日期</el-text>
         <div class="ml-3 w-30 mr-5">
           <!-- 日期选择组件（区间选择） -->
-          <el-date-picker v-model="pickDate" type="daterange" range-separator="至" start-placeholder="开始时间"
-                          end-placeholder="结束时间" size="default" />
+          <el-date-picker style="top: 3px" v-model="pickDate" type="daterange" range-separator="至" start-placeholder="开始时间"
+            end-placeholder="结束时间" :shortcuts="shortcuts" size="default" @change="datepickerChange" />
+
+
         </div>
 
-        <el-button type="primary" class="ml-3" :icon="Search">查询</el-button>
-        <el-button class="ml-3" :icon="RefreshRight">重置</el-button>
+        <el-button type="primary" class="ml-3" :icon="Search" @click="getTableData">查询</el-button>
+
+        <el-button class="ml-3" :icon="RefreshRight" @click="reset">重置</el-button>
       </div>
     </el-card>
 
@@ -51,7 +54,8 @@
     <div class="mt-10 flex justify-center">
       <el-pagination v-model:current-page="current" v-model:page-size="size" :page-sizes="[10, 20, 50]"
                      :small="false" :background="true" layout="total, sizes, prev, pager, next, jumper"
-                     :total="total" />
+                     :total="total" @size-change="handleSizeChange" @current-change="getTableData" />
+
     </div>
 
   </div>
@@ -60,30 +64,95 @@
 <script setup>
 // 引入所需图标
 import { Search, RefreshRight } from '@element-plus/icons-vue'
-import { ref } from 'vue';
+import { ref, reactive } from 'vue'
+import moment from 'moment'
+import { getCategoryPageList } from '@/api/admin/category'
 
 // 分页查询的分类名称
 const searchCategoryName = ref('')
 // 日期
 const pickDate = ref('')
 
-const tableData = [
-  {
-    createTime: '2016-05-03 12:00:00',
-    name: 'Java'
-  },
-  {
-    createTime: '2016-05-03 12:00:00',
-    name: 'Minio'
-  },
-]
-
+// 表格数据
+const tableData = ref([])
 // 当前页码，给了一个默认值 1
 const current = ref(1)
 // 总数据量，给了个默认值 0
 const total = ref(0)
 // 每页显示的数据量，给了个默认值 10
 const size = ref(10)
+
+// 查询条件：开始结束时间
+const startDate = reactive({})
+const endDate = reactive({})
+
+// 监听日期组件改变事件，并将开始结束时间设置到变量中
+const datepickerChange = (e) => {
+  startDate.value = moment(e[0]).format('YYYY-MM-DD')
+  endDate.value = moment(e[1]).format('YYYY-MM-DD')
+
+  console.log('开始时间：' + startDate.value + ', 结束时间：' + endDate.value)
+}
+
+
+const shortcuts = [
+  {
+    text: '最近一周',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+      return [start, end]
+    },
+  },
+  {
+    text: '最近一个月',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+      return [start, end]
+    },
+  },
+  {
+    text: '最近三个月',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+      return [start, end]
+    },
+  },
+]
+
+// 获取分页数据
+function getTableData() {
+  // 调用后台分页接口，并传入所需参数
+  getCategoryPageList({current: current.value, size: size.value, startDate: startDate.value, endDate: endDate.value, name: searchCategoryName.value})
+      .then((res) => {
+        if (res.success == true) {
+          tableData.value = res.data
+          current.value = res.current
+          size.value = res.size
+          total.value = res.total
+        }
+      })
+}
+getTableData()
+
+// 每页展示数量变更事件
+const handleSizeChange = (chooseSize) => {
+  size.value = chooseSize
+  getTableData()
+}
+
+// 重置查询条件
+const reset = () => {
+  searchCategoryName.value = ''
+  pickDate.value = ''
+  startDate.value = null
+  endDate.value = null
+}
 
 </script>
 
