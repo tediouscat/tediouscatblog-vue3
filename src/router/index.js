51f8a0995d82dc4aa2bfb1c5e768f59a1045fd59
tediouscat <515889839@qq.com>
2025-03-30 01:24:46 +0800

    前台分类-文章列表页: 样式布局开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,51 @@
-import Index from "@/pages/frontend/index.vue";
-import {createRouter,createWebHashHistory} from "vue-router";
-import Login from "@/pages/admin/login.vue";
-import AdminIndex from "@/pages/admin/index.vue"
-import Admin from "@/layouts/admin/admin.vue";
-import AdminCategoryList from "@/pages/admin/category-list.vue"
-import AdminArticleList from "@/pages/admin/article-list.vue"
-import AdminTagList from "@/pages/admin/tag-list.vue";
-import AdminBlogSetting from "@/pages/admin/blog-settings.vue"
+import Index from '@/pages/frontend/index.vue'
 import ArchiveList from '@/pages/frontend/archive-list.vue'
-import CategoryList from "@/pages/frontend/category-list.vue";
+import CategoryList from '@/pages/frontend/category-list.vue'
+import CategoryArticleList from '@/pages/frontend/category-article-list.vue'
+import Login from '@/pages/admin/login.vue'
+import AdminIndex from '@/pages/admin/index.vue'
+import AdminArticleList from '@/pages/admin/article-list.vue'
+import AdminCategoryList from '@/pages/admin/category-list.vue'
+import AdminTagList from '@/pages/admin/tag-list.vue'
+import AdminBlogSettings from '@/pages/admin/blog-settings.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Admin from '@/layouts/admin/admin.vue'
 
+// 统一在这里声明所有路由
 const routes = [
     {
-        path: "/",
-        component: Index,
-        meta: {
-            title: 'TediouscatBlog首页'
+        path: '/', // 路由地址，首页
+        component: Index, // 对应组件
+        meta: { // meta 信息
+            title: 'Weblog 首页' // 页面标题
         }
     },
-    // 省略...
+    {
+        path: '/archive/list', // 归档页
+        component: ArchiveList,
+        meta: { // meta 信息
+            title: 'Weblog 归档页'
+        }
+    },
+    {
+        path: '/category/list', // 分类页
+        component: CategoryList,
+        meta: { // meta 信息
+            title: 'Weblog 分类页'
+        }
+    },
+    {
+        path: '/category/article/list', // 分类文章页
+        component: CategoryArticleList,
+        meta: { // meta 信息
+            title: 'Weblog 分类文章页'
+        }
+    },
     {
         path: '/login', // 登录页
         component: Login,
         meta: {
-            title: 'TediouscatBlog登录页'
+            title: 'Weblog 登录页'
         }
     },
     {
@@ -60,35 +82,25 @@ const routes = [
                 }
             },
             {
-                path: "/admin/blog/setting",
-                component: AdminBlogSetting,
+                path: "/admin/blog/settings",
+                component: AdminBlogSettings,
                 meta: {
                     title: '博客设置'
                 }
             },
         ]
 
-    },
-    {
-        path: '/archive/list', // 归档页
-        component: ArchiveList,
-        meta: { // meta 信息
-            title: 'Weblog 归档页'
-        }
-    },
-    {
-        path: '/category/list', // 分类页
-        component: CategoryList,
-        meta: { // meta 信息
-            title: 'Weblog 分类页'
-        }
-    },
+    }
 ]
 
-const routers =createRouter({
+// 创建路由
+const router = createRouter({
+    // 指定路由的历史管理方式，hash 模式指的是 URL 的路径是通过 hash 符号（#）进行标识
     history: createWebHashHistory(),
+    // routes: routes 的缩写
     routes,
 })
 
+// 暴露出去
+export default router
 
-export default routers;
